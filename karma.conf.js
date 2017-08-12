b78b4b30feebd536eeeea4186ca34795816c724f
Gregory Anne <englishblazere@free.fr>
2017-08-12 02:30:25 +0200

    try without no-sandbox flag

--- karma.conf.js
+++ karma.conf.js
@@ -58,16 +58,9 @@ module.exports = function (config) {
             'karma-jasmine'
         ],
 
-        customLaunchers: {
-            Chrome_gitlab: {
-                base: 'ChromeHeadless',
-                flags: ['--no-sandbox']
-            }
-        },
-
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ['Chrome_gitlab'],
+        browsers: ['ChromeHeadless'],
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
